a9a9c15ae8234473ca3d13cb2194fe31158a7021
NizTheDev <arehman08@hotmail.com>
2022-11-30 00:55:40 +0500

    feat: done for the day

--- screens/StartGame/StartGame.screen.styles.ts
+++ screens/StartGame/StartGame.screen.styles.ts
@@ -1,5 +1,24 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-
+  mainContainer: {
+    backgroundColor: "#eee",
+    padding: 20,
+    marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 10,
+  },
+  input: {
+    backgroundColor: "#fff",
+    paddingHorizontal: 20,
+    paddingVertical: 4,
+    marginBottom: 20,
+    borderRadius: 4,
+    fontSize: 26,
+    fontWeight: "bold",
+    color: "rgba(83, 196, 247, 1)",
+    letterSpacing: 1,
+    textAlign: "center",
+    width: "25%"
+  }
 })
--- screens/StartGame/StartGame.screen.tsx
+++ screens/StartGame/StartGame.screen.tsx
@@ -1,12 +1,17 @@
 import { TextInput, View } from "react-native";
 import { CustomButton } from "../../components/CustomButton/CustomButton";
+import { styles } from "./StartGame.screen.styles";
 
 type Props = {};
 
 export const StartGameScreen = (props: Props) => {
   return (
-    <View>
-      <TextInput />
+    <View style={styles.mainContainer}>
+      <TextInput
+        style={styles.input}
+        maxLength={2}
+        keyboardType="decimal-pad"
+      />
       <CustomButton title="Reset" />
       <CustomButton title="Confirm" />
     </View>
