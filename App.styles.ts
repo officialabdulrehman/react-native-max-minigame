ac1ff5dfd061c59cfe9ed64d903b446a1c69d871
NizTheDev <arehman08@hotmail.com>
2022-11-30 23:31:25 +0500

    feat: added a background image to the whole app and some styles to root view of the app in App

--- App.styles.ts
+++ App.styles.ts
@@ -1,10 +1,13 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({
-  container: {
+export const styles = StyleSheet.create({
+  root: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
+  backgroundImage: {
+    opacity: 0.75,
+  }
 });
--- App.tsx
+++ App.tsx
@@ -1,5 +1,18 @@
+import { ImageBackground, View } from "react-native";
+import { styles } from "./App.styles";
 import { StartGameScreen } from "./screens/StartGame/StartGame.screen";
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <View style={styles.root}>
+      <ImageBackground
+        source={require("./assets/images/rin_01.jpg")}
+        resizeMode="cover"
+        style={styles.root}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </View>
+  );
 }
